2eb869c73306123c43a97c1f02fdd48377683b72
Victory <bedford490@gmail.com>
2023-08-08 13:09:42 +0100

    updated

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const fs = require('fs');
 
+// Check if the correct number of command-line arguments is provided
+if (process.argv.length !== 3) {
+  console.log('Usage: node converter.js input.txt');
+  process.exit(1);
+}
+
+const inputFilePath = process.argv[2]; // Get the input file path from command-line arguments
+
 // Read the TXT file
-fs.readFile('input.txt', 'utf8', (err, data) => {
+fs.readFile(inputFilePath, 'utf8', (err, data) => {
   if (err) {
     console.error('Error reading the TXT file:', err);
     return;
@@ -11,12 +19,12 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
   const lines = data.split('\n');
   const csvData = lines.map(line => {
     const values = line.split('\t'); // Modify delimiter based on your TXT file
-    return values;
-      
+    return values.join(','); // Convert values to CSV format
   }).join('\n'); // Join lines to create CSV content
 
   // Write to CSV File
-  fs.writeFile('output.csv', csvData, 'utf8', err => {
+  const outputFilePath = 'output.csv'; // You can customize the output file path if needed
+  fs.writeFile(outputFilePath, csvData, 'utf8', err => {
     if (err) {
       console.error('Error writing to CSV file:', err);
       return;
@@ -24,4 +32,3 @@ fs.readFile('input.txt', 'utf8', (err, data) => {
     console.log('CSV file created successfully!');
   });
 });
-
