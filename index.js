74ef3616fe5ab42c938f37e3ad80ee84cbc93b86
Victory <bedford490@gmail.com>
2023-08-08 07:36:39 +0100

    assignment

---
+++ index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+// Read the TXT file
+fs.readFile('input.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading the TXT file:', err);
+    return;
+  }
+
+  // Parse TXT Content
+  const lines = data.split('\n');
+  const csvData = lines.map(line => {
+    const values = line.split('\t'); // Modify delimiter based on your TXT file
+    for(value in values){
+      return value + ","; // Convert values to CSV format
+    }
+      
+  }).join('\n'); // Join lines to create CSV content
+
+  // Write to CSV File
+  fs.writeFile('output.csv', csvData, 'utf8', err => {
+    if (err) {
+      console.error('Error writing to CSV file:', err);
+      return;
+    }
+    console.log('CSV file created successfully!');
+  });
+});
+
